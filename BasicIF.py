8035780572456d9a2c0d3e553f38522c656f8d26
Your Name <your.email@example.com>
2026-07-18 12:49:45 +0200

    Add section pass verification to BasicIF.py

--- BasicIF.py
+++ BasicIF.py
@@ -1,5 +1,12 @@
 age = int(input("Enter your age: "))
+section_pass =input("Do you have a section pass? (yes/no): ").strip().lower()
+
 if age >= 18:
     print("You are an adult. ")
 else:
     print("You are a minor. ")
+
+if section_pass ==yes:
+    print("You have a section pass. ")
+else:
+    print("You do not have a section pass. ")
